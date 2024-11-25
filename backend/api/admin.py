7fe2271352bd909b924ace48ca00694c0dd99c46
shra <modak2007035@gmail.com>
2024-11-25 13:18:36 +0600

    user and profile model added(58)

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from api import models as api_models
+
+admin.site.register(api_models.User)
+admin.site.register(api_models.Profile)
 
 # Register your models here.
