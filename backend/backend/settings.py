e0f0be15f1a887f3cd1fdb3f833936cb0e9abf92
sujoy <sujoysadhu333@gmail.com>
2025-05-28 18:39:47 +0600

    secret key isssues fixed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,25 +11,34 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from pathlib import Path
 from datetime import timedelta
-# from environs import Env
 import os
-GOOGLE_API_KEY = os.environ.get('GOOGLE_API_KEY', 'AIzaSyANdjlhcK0t29A5Lu3nVyF878T3MmsoZJs')
- 
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x)f!y0q(dkt3-=4qh*$#i_owf_a)e@)j@*^1ha)!vqfjwu*52#'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
+# Google API Key
+GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')
 
 # Application definition
 
@@ -298,10 +307,3 @@ LOGGING = {
         'level': 'DEBUG',
     },
 }
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sujoyy19@gmail.com'
-EMAIL_HOST_PASSWORD = 'bmak vgtu lmpq rafm'
-DEFAULT_FROM_EMAIL = 'sujoyy19@gmail.com'
